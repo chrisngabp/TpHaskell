20fe6658937cbc44c1ed00113197058686933ec0
Chris Panetta <Chris Panetta>
2020-06-04 19:19:06 -0300

    improvements for data structures

--- TpHaskell2Estructuras.hs
+++ TpHaskell2Estructuras.hs
@@ -1,41 +1,39 @@
 data Quizas a = Error String | OK a deriving (Show)
 
 instance Functor Quizas where
-	fmap _ (Error x) = Error x
-	fmap f (OK x) = OK $ f x
+    fmap _ (Error x) = Error x
+    fmap f (OK x) = OK $ f x
 
 instance Applicative Quizas where
-	pure x			= OK x
-	_ <*> Error x 	= Error x
-	Error x <*> _	= Error x
-	OK f <*> OK x	= OK $ f x
+    pure x			= OK x
+    _ <*> Error x 	= Error x
+    Error x <*> _	= Error x
+    OK f <*> OK x	= OK $ f x
 
 instance Monad Quizas where
-	Error x >>= _ = Error x
-	OK x >>= f 	  = f x
+    Error x >>= _ = Error x
+    OK x >>= f 	  = f x
 
 data Archivo = Archivo NombreM [Importacion] [Data] [Clase] [Instancia] [Funcion]
 
 instance Show Archivo where
-	show = undefined -- mostrarArchivo
+    show = undefined -- mostrarArchivo
 
 -- instance Eq Archivo where
 
 data Importacion = Importacion NombreM String (Maybe Comentario) -- "Data.List" "import Data.List (inits)"
 
 data Codigo = CD Data | CC Clase | CI Instancia | CF Funcion
--- Que seria codigo entonces? Archivo es un array de codigo?
--- Importacion es un codigo?
 
 data Data = Data NombreDeDato String
 type NombreDeDato = String
 
-data Clase = Clase NombreClase SoloUnWhere Firma -- O array de firma?
+data Clase = Clase (Maybe HerenciaClase) NombreClase Firma (Maybe Where)
+type NombreClase = String
+type HerenciaClase = String 
 
-data Instancia = Instancia TipoInstancia NombreDeDato SoloUnWhere [Funcion] -- Funcion o funcionos? O son otra cosa?
+data Instancia = Instancia NombreClase NombreDeDato Where
 type TipoInstancia = String
-
-type SoloUnWhere = String
 
 -- INICIO Datas y types de Funcion
 
@@ -45,26 +43,22 @@ type Firma = String -- Sinonimos (type)
 data Patron = Patron (Either Bloque [Pipe]) (Maybe Where) (Maybe Comentario)
 type Where = [Funcion]
 data Bloque = 	Expresion Resultado | 
-				LetIn [Funcion] Bloque | 
-				IfThenElse Bloque Bloque Bloque | 
-				Case [Bloque] |
-				Do [Funcion]
--- seguir completando Bloque
+                LetIn [Funcion] Bloque | 
+                IfThenElse Bloque Bloque Bloque | 
+                Case [Bloque]
 
 type Resultado = String
 type ResultadoBool = String
 data Pipe = Pipe ResultadoBool Bloque 
 
--- seguir completando, podria haber un ResultadoString? ResultadoInt?
-
 -- FIN Datas y types de Funcion
 
 type Comentario = String
-class ConComentario where
-	comentario :: a -> Maybe Comentario
+class ConComentario a where
+    comentario :: a -> Maybe Comentario
 
-newtype NombreM = NombreM String -- Nombre válido para módulos, datos, etc.
-newtype NombreF = NombreF String | OperadorF String -- Nombre válido para funciones
+data NombreM = NombreM String -- Nombre válido para módulos, datos, etc.
+data NombreF = NombreF String | OperadorF String -- Nombre válido para funciones
 data Nombre = NM NombreM | NF NombreF
 
 data Opciones = Opciones {largoLinea :: Int, anchoTab :: Int, comentarioInline :: Bool, compacto :: Bool, colores :: Bool} -- Pueden agregar otros parámetros.
