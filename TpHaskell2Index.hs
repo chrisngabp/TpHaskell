116dd3020a1ac3121ff3b598d8f92f404016aa44
Chris Panetta <Chris Panetta>
2020-06-04 22:41:28 -0300

    updating data structures and adding comment functions

---
+++ TpHaskell2Index.hs
@@ -0,0 +1,192 @@
+module TpHaskell2Index where
+
+data Quizas a = Error String | OK a deriving (Show)
+
+instance Functor Quizas where
+    fmap _ (Error x) = Error x
+    fmap f (OK x) = OK $ f x
+
+instance Applicative Quizas where
+    pure x			= OK x
+    _ <*> Error x 	= Error x
+    Error x <*> _	= Error x
+    OK f <*> OK x	= OK $ f x
+
+instance Monad Quizas where
+    Error x >>= _ = Error x
+    OK x >>= f 	  = f x
+
+data Archivo = Archivo NombreM [Importacion] [Data] [Clase] [Instancia] [Funcion]
+
+instance Show Archivo where
+    show = undefined -- mostrarArchivo
+
+-- instance Eq Archivo where
+
+data Importacion = Importacion NombreM String (Maybe Comentario) -- "Data.List" "import Data.List (inits)"
+
+data Codigo = CD Data | CC Clase | CI Instancia | CF Funcion
+
+data Data = Data NombreDeDato String
+type NombreDeDato = String
+
+data Clase = Clase (Maybe HerenciaClase) NombreClase Firma (Maybe Where)
+type NombreClase = String
+type HerenciaClase = String 
+
+data Instancia = Instancia NombreClase NombreDeDato Where
+type TipoInstancia = String
+
+-- INICIO Datas y types de Funcion
+
+data Funcion = Funcion NombreF (Maybe Firma) [Patron] (Maybe Comentario)
+type Firma = String -- Sinonimos (type)
+
+data Patron = Patron (Either Bloque [Pipe]) (Maybe Where) (Maybe Comentario)
+type Where = [Funcion]
+data Bloque = 	Expresion Resultado | 
+                LetIn [Funcion] Bloque | 
+                IfThenElse Bloque Bloque Bloque | 
+                Case [Bloque]
+
+type Resultado = String
+type ResultadoBool = String
+data Pipe = Pipe ResultadoBool Bloque 
+
+-- FIN Datas y types de Funcion
+
+type Comentario = Maybe String
+class ConComentario a where
+    comentario :: a -> Maybe Comentario
+--    agregoComentarios :: a -> c -> c
+
+--instance ConComentario Funcion where
+--    agregoComentarios c' (Funcion nf mf lp mc) = Funcion nf mf lp (maybeConcatenoComentario c' mc)
+
+data NombreM = NombreM String -- Nombre válido para módulos, datos, etc.
+data NombreF = NombreF String | OperadorF String -- Nombre válido para funciones
+data Nombre = NM NombreM | NF NombreF
+
+data Opciones = Opciones {largoLinea :: Int, anchoTab :: Int, comentarioInline :: Bool, compacto :: Bool, colores :: Bool} -- Pueden agregar otros parámetros.
+
+{-
+EsNombre: Contiene a NombreM y NombreF
+ConNombre: Clase con los datos que tienen nombre.
+ConComentario: Clase con los datos que pueden contener comentario.
+-}
+
+
+{-
+-
+- INICIO DE FUNCIONES
+-
+-}
+
+-- CLASE 1 JUNIO
+data Comentado a = Comentado String a
+
+comentarioAString :: Comentario -> String
+comentarioAString Nothing = ""
+comentarioAString (Just x) = x
+
+stringAComentario :: String -> Comentario
+stringAComentario [] = Nothing
+stringAComentario x = Just x
+
+--Funcion (stringANombreF nom) .. .. (stringAComentario str)
+
+-- lineas = lines
+lineas :: String -> [String]
+lineas ""           = []
+lineas xs
+    | null xs'      = [xs]
+    | otherwise     = l : lineas (tail xs')
+    where (l,xs') = break (=='\n') xs
+
+filtroNulas :: [[a]] -> [[a]]
+filtroNulas = filter (not . null) -- comentario
+data Estoy = Normal | CML | CIL | Comillas
+data TipoCodigo = TCComIL Char | TCComML Char | TCCod Char deriving (Show)
+
+dCom :: String -> Quizas [TipoCodigo]
+dCom xs                 = dCom' Normal xs --
+
+dCom' :: Estoy -> String -> Quizas [TipoCodigo]
+dCom' CML []                 = Error "CML no terminado"
+dCom' Comillas []            = Error "Comillas no cerradas"
+dCom' _ []                   = OK []
+dCom' Normal ('{':'-':xs)    = dCom' CML xs
+dCom' CML ('-':'}':xs)       = dCom' Normal xs
+dCom' CML (x:xs)             = (TCComML x :) <$> dCom' CML xs
+dCom' CIL ('\n':xs)          = (TCCod '\n' :) <$> dCom' Normal xs
+dCom' Normal ('-':'-':xs)    = dCom' CIL xs
+dCom' CIL (x:xs)             = (TCComIL x :) <$> dCom' CIL xs
+dCom' Normal ('"':xs)        = (TCCod '"' :) <$> dCom' Comillas xs
+dCom' Comillas ('"':xs)      = (TCCod '"' :) <$> dCom' Normal xs
+dCom' Normal (x:xs)          = (TCCod x :) <$> dCom' Normal xs
+dCom' Comillas (x:xs)        = (TCCod x :) <$> dCom' Comillas xs
+
+--(:) :: a -> Quizas [a] -> Quizas [a]
+-- <$> = `fmap`
+--
+esCodigo :: TipoCodigo -> Bool
+esCodigo (TCCod _) = True
+esCodigo _          = False
+
+tipoCodigoAChr :: TipoCodigo -> Char
+tipoCodigoAChr (TCCod x) = x
+tipoCodigoAChr (TCComIL x) = x
+tipoCodigoAChr (TCComML x) = x
+
+fromQuizas (OK x) = x
+{-
+class ConComentario a where
+    comentario :: a -> Maybe Comentario
+    agregoComentario :: Comentario -> a -> a
+-}
+maybeConcateno :: String -> Maybe String -> Maybe String
+maybeConcateno c' Nothing = Just c'
+maybeConcateno c' (Just c) = Just $ c ++ "\n" ++ c'
+{-
+instance ConComentario Funcion where
+    agregoComentario c' (Funcion f x w mc) = Funcion f x w (maybeConcateno c' mc)
+-}
+
+--leoArchivo
+-- "f x = 1 \ng y = 2"
+-- [TCACod "f x = 1 ",TCACom "comentario",TCACod "g y = 2"]
+agrupoComentarios = aC
+
+data TipoCodigoAg = TCACod String | TCAComIL String | TCAComML String deriving (Show)
+
+aC :: [TipoCodigo] -> [TipoCodigoAg]
+aC []               = []
+aC xs'@(TCCod x:xs) = map TCACod linIni ++ aC fin
+    where (ini,fin) = break (not . esCodigo) xs'
+          ini'      = map tipoCodigoAChr ini
+          linIni    = filter (not . null) $ lineas ini'
+aC xs'@(TCComIL x:xs) = TCAComIL (map tipoCodigoAChr ini) : aC fin
+    where (ini,fin) = break esCodigo xs'
+aC xs'@(TCComML x:xs) = TCAComML (map tipoCodigoAChr ini) : aC fin
+    where (ini,fin) = break esCodigo xs'
+
+data TipoCodigoAg2 = TCACod2 String Comentario deriving (Show)
+
+aC2 :: [TipoCodigoAg] -> [TipoCodigoAg2]
+aC2 (TCAComML x:TCACod y:TCAComIL x':xs) = TCACod2 y (Just (x ++ "\n" ++ x')) : aC2 xs
+aC2 (TCAComML x:TCACod y:xs) = TCACod2 y (Just x) : aC2 xs
+aC2 (TCACod y:TCAComIL x:xs) = TCACod2 y (Just x) : aC2 xs
+aC2 (TCAComIL x:TCAComML y:xs) = aC2 (TCAComML (x ++ "\n" ++ y) : xs)
+aC2 (TCAComML x:TCAComIL y:xs) = aC2 (TCAComML (x ++ "\n" ++ y) : xs)
+aC2 (TCACod x:xs) = TCACod2 x Nothing : aC2 xs
+aC2 (_:xs)             = aC2 xs
+aC2 []                 = []
+
+
+maybeConcatenoComentario :: String -> Maybe String -> Maybe String
+maybeConcatenoComentario c' Nothing = Just c'
+maybeConcatenoComentario c' (Just c) = Just $ c ++ "\n" ++ c'
+
+
+-- agrupoComentario <$> dCom "string con cosas"
+
