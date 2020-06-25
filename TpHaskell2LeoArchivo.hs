d0edd9c25ae112cf0d61632a03f3e1dfae8a9598
Chris Panetta <Chris Panetta>
2020-06-25 13:11:46 -0300

    separating leoarchivo module

--- TpHaskell2LeoArchivo.hs
+++ TpHaskell2LeoArchivo.hs
@@ -1,16 +1,18 @@
-module TpHaskell2Index where
+module TpHaskell2LeoArchivo where
 
 import LoadFile
 import TpHaskell2Estructuras
 import Debug.Trace
 
+
 {-
--
-- INICIO DE FUNCIONES
--
+    FUNCIONES PARA LECTURA DEL ARCHIVO
 -}
 
--- Generales
+{-
+    Lectura de comentario
+-}
+
 esCodigo :: TipoCodigo -> Bool
 esCodigo (TCCod _) = True
 esCodigo _          = False
@@ -31,32 +33,13 @@ lineas xs
 
 filtroNulas :: [[a]] -> [[a]]
 filtroNulas = filter (not . null) -- comentario
-data Estoy = Normal | CML | CIL | Comillas
-data TipoCodigo = TCComIL Char | TCComML Char | TCCod Char deriving (Show)
 
 maybeConcateno :: String -> Maybe String -> Maybe String
 maybeConcateno c' Nothing = Just c'
 maybeConcateno c' (Just c) = Just $ c ++ "\n" ++ c'
 
--- Comentarios
-data Comentado a = Comentado String a
-
-comentarioAString :: Comentario -> String
-comentarioAString Nothing = ""
-comentarioAString (Just x) = x
-
-stringAComentario :: String -> Comentario
-stringAComentario [] = Nothing
-stringAComentario x = Just x
-
-
-{-
-    FUNCIONES PARA LECTURA DEL ARCHIVO
--}
-
-{-
-    Lectura de comentario
--}
+data Estoy = Normal | CML | CIL | Comillas
+data TipoCodigo = TCComIL Char | TCComML Char | TCCod Char deriving (Show)
 
 dCom :: String -> Quizas [TipoCodigo]
 dCom xs                 = dCom' Normal xs --
@@ -483,76 +466,3 @@ archivoArmado = armarArchivo (dCod $ pruebaArchivoALista) archivoVacio
 
 
 
-
-
-{-
-
-ACA NO MIRO NADA
-
--}
-
-archivoPruebaModulo = Archivo (NombreM "TpHaskell2Index") [] [] [] [] []
-modulo :: Archivo -> Quizas NombreM -- Devuelve el módulo
-modulo (Archivo nm _ _ _ _ _) = OK nm
--- para probar en GHCI : modulo $ archivoPruebaModulo
-
-importPruebaImports = Importacion (NombreM "Data.List (inits)") (Just "prueba") --"-- agrego lista"
-importPruebaImports2 = Importacion (NombreM "Data.List") Nothing
-archivoPruebaImports = Archivo (NombreM "") [importPruebaImports, importPruebaImports2] [] [] [] []
-imports :: Archivo -> [Importacion] -- Módulos que importa
-imports (Archivo _ im _ _ _ _) = im
--- para probar en GHCI : imports $ archivoPruebaImports
-
-dataPruebaData = Data "Estoy" "= Normal | CML | CIL | Comillas" Nothing
-archivoPruebaDatas = Archivo (NombreM "") [] [dataPruebaData] [] [] []
-datas :: Archivo -> [Data] -- Tipos de dato que genera
-datas (Archivo _ _ d _ _ _) = d
--- para probar en GHCI : datas $ archivoPruebaDatas
-
-clasePruebaClases = Clase (Just "(Monad m, Monad (t m))") "Transform" "t m" [] Nothing
-archivoPruebaClases = Archivo (NombreM "") [] [] [clasePruebaClases] [] []
-clases :: Archivo -> [Clase] -- Clases que genera
-clases (Archivo _ _ _ c _ _) = c
--- para probar en GHCI : clases $ archivoPruebaClases
-
--- TODO: completar estas que quedaron
-{-
-funciones :: Archivo -> [Funciones] -- Funciones definidas
-instancias :: NombreM -> Archivo -> Quizas [Instancias] -- Devuelve las instancias definidas de un tipo de dato
-nombres :: Archivo -> [Nombre] -- Lista de clases, datos, funciones, etc incluidas (solo de primer nivel).
--}
-
-bloqueFuncionPrueba1 = Expresion "a | a < 5 = a + 1"
-bloqueFuncionPrueba2 = Expresion "a = a"
-patronFuncionPrueba = Patron (Left bloqueFuncionPrueba1) [] Nothing
-patronFuncionPrueba2 = Patron (Left bloqueFuncionPrueba2 ) [] (Just "-- comento esto")
-funcionPruebaAgregoFuncion = Funcion (NombreF "miFuncion") (Just "a -> a") [patronFuncionPrueba, patronFuncionPrueba2] Nothing
-archivoConFuncionAgregada = fromQuizas $ agregoFuncion funcionPruebaAgregoFuncion archivoPruebaModulo
-
-agregoFuncion :: Funcion -> Archivo -> Quizas Archivo -- Agrega una función
--- tiene un Quizas archivo porque si ya existe la funcion devuelvo un error
-agregoFuncion f (Archivo nm im d c ins fa) | yaExisteFuncion f fa = Error "La funcion ya existe"
-agregoFuncion f (Archivo nm im d c ins fa) = OK (Archivo nm im d c ins (f:fa))
-
-yaExisteFuncion :: Funcion -> [Funcion] -> Bool
-yaExisteFuncion f (fa:farest) = if funcionesIguales f fa then True else yaExisteFuncion f farest
-yaExisteFuncion _ _ = False
-
-funcionesIguales :: Funcion -> Funcion -> Bool
-funcionesIguales (Funcion nomf1 _ _ _) (Funcion nomf2 _ _ _) = (nomf1 == nomf2)
-
-sacoFuncion :: NombreF -> Archivo -> Archivo -- Devuelve el Archivo sacando una función
-sacoFuncion f (Archivo nm im d c ins fa) = Archivo nm im d c ins (sacoFuncion' (creoFuncionSoloConNombre f) fa)
-
-sacoFuncion' _ []                 = []
-sacoFuncion' x@(Funcion f1 _ _ _) (y@(Funcion f2 _ _ _):ys) | f1 == f2    = sacoFuncion' x ys
-                                                            | otherwise   = y : sacoFuncion' x ys
-
-                                                            -- sacar la recursividad
-
-creoFuncionSoloConNombre :: NombreF -> Funcion
-creoFuncionSoloConNombre f = Funcion f Nothing [] Nothing
-
--- Esto no es necesario sacoFuncion f (Archivo nm im d c ins fa) = Error "La funcion no existe"
-
---sacoFuncion :: NombreF -> Archivo -> Archivo -- Devuelve el Archivo sacando una función
